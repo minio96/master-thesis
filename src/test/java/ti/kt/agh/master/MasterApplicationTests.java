7f1c963aa6664903ecda8b91cbc020a040265943
minio96 <mi.sypek96@gmail.com>
2020-04-25 18:02:56 +0200

    Initial commit - repo setup

---
+++ src/test/java/ti/kt/agh/master/MasterApplicationTests.java
@@ -0,0 +1,13 @@
+package ti.kt.agh.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
