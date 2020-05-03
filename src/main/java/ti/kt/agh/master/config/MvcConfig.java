fe8ae730cdb6b480f4071ec8531bef84d64dc8fa
minio96 <mi.sypek96@gmail.com>
2020-05-03 16:15:37 +0200

    - basic authentication setup

---
+++ src/main/java/ti/kt/agh/master/config/MvcConfig.java
@@ -0,0 +1,11 @@
+package ti.kt.agh.master.config;
+
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+}
